f1e6a3228d5dc550a4c5581a5c82738b7dbeba5c
Jonathan <jonathanharyono4@gmail.com>
2026-02-24 20:57:40 +0700

    Instantiated a caller and call more method on DataDosen14.java to answer number 2

--- Praktikum03/DosenDemo14.java
+++ Praktikum03/DosenDemo14.java
@@ -2,6 +2,7 @@ package Praktikum03;
 
 public class DosenDemo14 {
     public static void main(String[] args) {
+
         Dosen14[] arrayOfDosen14 = new Dosen14[3];
 
         for (int i = 0; i < 3; i++) {
@@ -12,9 +13,27 @@ public class DosenDemo14 {
         int i = 0;
 
         for (Dosen14 dosen14 : arrayOfDosen14) {
-            arrayOfDosen14[i].showData(i);
+            dosen14.showData(i);
             i++;
         }
+
+
+        // BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2
+        System.out.println("\n-------------------------------------------------------------------------------------------------\n");
+        // BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2 BATAS TUGAS NOMOR 2
+
+
+        DataDosen14 dataDosenCaller = new DataDosen14();
+
+        dataDosenCaller.dataSemuaDosen(arrayOfDosen14);
+
+        dataDosenCaller.jumlahDosenPerJenisKelamin(arrayOfDosen14);
+
+        dataDosenCaller.rerataUsiaDosenPerJenisKelamin(arrayOfDosen14);
+
+        dataDosenCaller.infoDosenPalingTua(arrayOfDosen14);
+
+        dataDosenCaller.infoDosenPalingMuda(arrayOfDosen14);
 
     }
 }
