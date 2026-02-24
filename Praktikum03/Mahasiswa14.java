25db2182b50b6103f5f525b64425409e2b8b5a4b
Jonathan <jonathanharyono4@gmail.com>
2026-02-24 08:53:28 +0700

    Adding cetakInfo() method

--- Praktikum03/Mahasiswa14.java
+++ Praktikum03/Mahasiswa14.java
@@ -5,4 +5,14 @@ public class Mahasiswa14 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    void cetakInfo(int index) {
+        System.out.println("Data mahasiswa ke-" + (index + 1));
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("--------------------------------------\n");
+    }
 }
+
