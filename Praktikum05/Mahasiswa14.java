d41c38656bcedb8ca189f5f8d7b111dffcda4084
Jonathan <jonathanharyono4@gmail.com>
2026-03-31 10:12:32 +0700

    Modifikasi data dinamis

--- Praktikum05/Mahasiswa14.java
+++ Praktikum05/Mahasiswa14.java
@@ -10,12 +10,13 @@ public class Mahasiswa14 {
     }
 
     // Konstruktor berparameter (dibuat ada yang nama var parameter inputnya sama ada yang tidak)
-    Mahasiswa14(String nm, String name, String kls, double ip) {
+    /* Mahasiswa14(String nm, String name, String kls, double ip) {
         nim = nm;
         nama = name;
         ipk = ip;
         kelas = kls;
     }
+    */
 
     void tampilInformasi() {
         System.out.println("Nama    : " + nama);
--- Praktikum05/MahasiswaBerprestasi14.java
+++ Praktikum05/MahasiswaBerprestasi14.java
@@ -15,15 +15,16 @@ public class MahasiswaBerprestasi14 {
     }
 
     void tampil() {
-        for (Mahasiswa14 m : listMhs) {
-            m.tampilInformasi();
-            System.out.println("---------------------------------------");
-        }
+    for (int i = 0; i < idx; i++) {
+        listMhs[i].tampilInformasi();
+        System.out.println("---------------------------------------");
     }
+}
 
     void bubbleSort() {
-        for (int i = 0; i < listMhs.length-1; i++) {
-            for (int j = 1; j < listMhs.length-i; j++) {
+    // Ganti listMhs.length menjadi idx
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
                 if (listMhs[j].ipk > listMhs[j-1].ipk) {
                     Mahasiswa14 tmp = listMhs[j];
                     listMhs[j] = listMhs[j-1];
