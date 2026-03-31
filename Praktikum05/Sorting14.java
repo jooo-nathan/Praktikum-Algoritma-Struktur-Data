59ed777b8da720f868f377f63829e2029d367439
Jonathan <jonathanharyono4@gmail.com>
2026-03-31 07:57:38 +0700

    Sorting - Insertion Sort

--- Praktikum05/Sorting14.java
+++ Praktikum05/Sorting14.java
@@ -49,4 +49,17 @@ public class Sorting14 {
             
         }
     }
+
+    void insertionSort() {
+        for (int i = 1; i < jumData; i++) {
+            int temp = data[i];
+            int j = i-1;
+
+            while (j >= 0 && data[j] > temp) {
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = temp;
+        }
+    }
 }
