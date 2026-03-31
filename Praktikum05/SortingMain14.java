f03136124db10666a553b313650ebd46f2a033d1
Jonathan <jonathanharyono4@gmail.com>
2026-03-31 07:42:09 +0700

    Sorting - Bubble Sort

---
+++ Praktikum05/SortingMain14.java
@@ -0,0 +1,17 @@
+package Praktikum05;
+
+public class SortingMain14 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+
+        Sorting14 dataurut1 = new Sorting14(a, a.length);
+
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();    
+    }
+    
+}
