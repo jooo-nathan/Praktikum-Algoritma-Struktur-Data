3822a5ec464e8147373b6e3e46b355ac09078b63
Jonathan <jonathanharyono4@gmail.com>
2026-03-31 10:41:33 +0700

    Renamed the folder from 05 to 06

---
+++ Praktikum06/Mahasiswa14.java
@@ -0,0 +1,27 @@
+package Praktikum06;
+
+public class Mahasiswa14 {
+    String nim, nama, kelas;
+    double ipk;
+
+    //Konstruktor default
+    Mahasiswa14() {
+
+    }
+
+    // Konstruktor berparameter (dibuat ada yang nama var parameter inputnya sama ada yang tidak)
+    /* Mahasiswa14(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+    */
+
+    void tampilInformasi() {
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : " + nim);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+    }
+}
---
+++ Praktikum06/MahasiswaBerprestasi14.java
@@ -0,0 +1,52 @@
+package Praktikum06;
+
+public class MahasiswaBerprestasi14 {
+    Mahasiswa14[] listMhs = new Mahasiswa14[5];
+    int idx;
+
+    void tambah (Mahasiswa14 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        }
+        else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+    void tampil() {
+    for (int i = 0; i < idx; i++) {
+        listMhs[i].tampilInformasi();
+        System.out.println("---------------------------------------");
+    }
+}
+
+    void bubbleSort() {
+    // Ganti listMhs.length menjadi idx
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
+                if (listMhs[j].ipk > listMhs[j-1].ipk) {
+                    Mahasiswa14 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = tmp;
+                }
+            }
+        }
+    }
+
+    void selectionSort() {
+        for (int i = 0; i < idx-1; i++) {
+            int idxMin = i;
+
+            for (int j = i+1; j < idx; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+
+            Mahasiswa14 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
+}
