cfce59ac125a7aaaf076a387d7b9beafce555f71
Jonathan <jonathanharyono4@gmail.com>
2026-04-07 09:41:05 +0700

    Modifikasi nomor 5

--- Praktikum07/MahasiswaBerprestasi14.java
+++ Praktikum07/MahasiswaBerprestasi14.java
@@ -47,7 +47,7 @@ public class MahasiswaBerprestasi14 {
             if (cari == listMhs[mid].ipk) {
                 return mid;
             }
-            else if (listMhs[mid].ipk > cari) {
+            else if (listMhs[mid].ipk < cari) {
                 return findBinarySearch(cari, left, mid-1);
             }
             else {
