ad13cdce7c3cee48dcd572d8dc00806e8b564510
Jonathan <jonathanharyono4@gmail.com>
2026-04-28 11:09:18 +0700

    Percobaan 2

---
+++ Jobsheet10/P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,164 @@
+package Jobsheet10.P2Jobsheet10;
+
+public class AntrianLayanan {
+    Mahasiswa[] data;
+    int front, rear, size, max;
+
+    public AntrianLayanan(int max) {
+        this.max = max;
+        this.data = new Mahasiswa[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        }
+        else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        }
+        else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i] + " " );
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+
+    public void clear() {
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        }
+        else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void tambahAntrian(Mahasiswa mhs) {
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa layaniMahasiswa() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return null;
+        }
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        }
+        else {
+            System.out.print("Mahasiswa terdepan : ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian : ");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i + 1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+
+    /* public boolean Enqueue(int dt) {
+        if (isFull()) {
+            System.out.println("Queue sudah penuh");
+            return false;
+        }
+        else {
+            if (isEmpty()) {
+                front = rear = 0;
+            }
+            else {
+                if (rear == max - 1) {
+                    rear = 0;
+                }
+                else {
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size++;
+            return true;
+        }
+    }
+
+    public int Dequeue() {
+        int dt = 0;
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        }
+        else {
+            dt = data[front];
+            size--;
+            if (isEmpty()) {
+                front = rear = -1;
+            }
+            else {
+                if (front == max - 1) {
+                    front = 0;
+                }
+                else {
+                    front++;
+                }
+            }
+        }
+        return dt;
+    }*/
+}
