11534f4abf41d9395552b7f0f70f8befd8a4bffe
Jonathan <jonathanharyono4@gmail.com>
2026-05-05 10:33:00 +0700

    Percobaan 2

--- Pertemuan11/SLLMain14.java
+++ Pertemuan11/SLLMain14.java
@@ -42,12 +42,13 @@ public class SLLMain14 {
     public static void main(String[] args) {
         SingleLinkedList14 sll = new SingleLinkedList14();
 
-        // Mahasiswa14 mhs1 = new Mahasiswa14("24212200", "Alvaro", "1A", 4.0);
-        // Mahasiswa14 mhs2 = new Mahasiswa14("23212201", "Bimon", "2B", 3.8);
-        //Mahasiswa14 mhs3 = new Mahasiswa14("22212202", "Cintia", "3C", 3.5);
-        // Mahasiswa14 mhs4 = new Mahasiswa14("21212203", "Dirga", "4D", 3.6);
+        
+        Mahasiswa14 mhs1 = new Mahasiswa14("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa14 mhs2 = new Mahasiswa14("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa14 mhs3 = new Mahasiswa14("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa14 mhs4 = new Mahasiswa14("21212203", "Dirga", "4D", 3.6);
 
-        /*
+        
         sll.print();
         sll.addFirst(mhs4);
         sll.print();
@@ -56,8 +57,8 @@ public class SLLMain14 {
         sll.insertAfter("Dirga", mhs3);
         sll.insertAt(2, mhs2);
         sll.print();
-        */
-
+        
+        /*
         System.out.println("=== PROGRAM LINKED LIST ===");
 
         int pilihan, subPilihan;
@@ -129,6 +130,19 @@ public class SLLMain14 {
             }
 
         } while (pilihan != 0);
-        
+        */
+
+        System.out.println("Data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("Data mahasiswa AN Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
+
     }
 }
